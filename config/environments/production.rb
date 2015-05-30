7553ca13abd15cf4e9ef15624c8ee06939cf6120
Ashish Mohan <ashish.spavj@gmail.com>
2015-05-30 17:11:00 +0200

    changed database and migration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # required for heroku set it actual host name 
+  config.action_mailer.default_url_options = { host: 'ovepinterest.herokuapp.com', port: 3000 }
 end
